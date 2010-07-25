449b7c2f20bf80c0b3d68c56bcfcbdee423d5858
Marien de Gelder <mariendegelder@gmail.com>
2010-07-25 16:50:04 +0200

    load more messages when reaching the bottom messages

--- ux.js
+++ ux.js
@@ -2,3 +2,20 @@
 jQuery('.branchContainer').focus( function () {
     jQuery(this).parents('.treeContainer').children('.treeHeader.closed').click();
 });
+
+// load more messages when reaching the bottom messages
+var selector = '.messageBlock > div:first-child';
+jQuery(selector).live( 'focus', function () {
+    var index = jQuery(selector).index(this);
+    var length = jQuery(selector).length;
+    if (index >= length-2) {
+        jQuery('#messages_widget_container').
+        var evt = document.createEvent('MouseEvents');
+        evt.initMouseEvent('click', true, true,
+            document.defaultView, 1, 0, 0, 0, 0, false,
+            false, false, false, 0, null);
+        jQuery('.more > a').each( function() {
+            this.dispatchEvent(evt);
+        });
+    }
+});
