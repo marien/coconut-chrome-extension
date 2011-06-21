91f8d13876bd62db4e23390b7c676bbde14b06d9
Marien de Gelder <mariendegelder@gmail.com>
2011-06-21 19:21:39 +0200

    Reload only when not in an input area as we would lose typed in text (fixed #16)

--- script.js
+++ script.js
@@ -145,13 +145,17 @@ chrome.extension.sendRequest({getOption: RELOADCENTER_INTERVAL_KEY}, function(re
 var reloadcenterinterval = response.RELOADCENTER_INTERVAL_KEY;
 if (reloadcenterinterval > 0) {
     function reloadCenter() {
-        var evt = document.createEvent('MouseEvents');
-        evt.initMouseEvent('click', true, true,
-            document.defaultView, 1, 0, 0, 0, 0, false,
-            false, false, false, 0, null);
-        jQuery('.reloadButton').each( function() {
-            this.dispatchEvent(evt);
-        });
+        // reload only when not in an input area as we would lose typed in text
+        if ( !/textarea|select/i.test( document.activeElement.nodeName ) &&
+             !/text|password/i.test( document.activeElement.type ) ) {
+            var evt = document.createEvent('MouseEvents');
+            evt.initMouseEvent('click', true, true,
+                document.defaultView, 1, 0, 0, 0, 0, false,
+                false, false, false, 0, null);
+            jQuery('.reloadButton').each( function() {
+                this.dispatchEvent(evt);
+            });
+        }
 		window.setTimeout(reloadCenter, reloadcenterinterval);
     }
 	window.setTimeout(reloadCenter, reloadcenterinterval);
